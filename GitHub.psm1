fe82802ebeb6b8353fda28d8605df7de015eec60
jeffbuenting <jeffbuenting@gmail.com>
2016-12-21 10:39:19 -0500

    update

---
+++ GitHub.psm1
@@ -0,0 +1,29 @@
+﻿Function Copy-GitHubItem {
+
+<#
+    .Synopsis
+        Downloads a file from GitHub
+
+    .Link
+        http://stackoverflow.com/questions/16597874/powershell-retrieve-file-from-github
+#>
+
+    [CmdletBinding()]
+    Param (
+        [String]$Url,
+
+        [PSCredential]$Credential
+    )
+
+    Begin {
+        # ----- Make the connection to GitHub
+        $WebClient = New-Object -TypeName System.Net.WebClient
+        $WebClient.Credentials = New-Object -TypeName System.Net.NetworkCredential $Credential.UserName,$Credential.GetNetworkCredential().Password 
+    }
+
+    Process {
+        # ----- Download 
+        invoke-expression ($Webclient.DownloadString($Url))
+    }
+}
+
